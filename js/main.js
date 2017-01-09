5031be97dea11131bc30b2cb2227e0a4561c10a4
Igor Melnyk <happy.melnyk@gmail.com>
2017-01-09 23:23:44 +0200

    add JQuery code to slow scroling to ankors

--- js/main.js
+++ js/main.js
@@ -6,4 +6,12 @@ $('document').ready ( function (){
 			$('.top-navigation').slideUp(200);
 		}
 	});
+	$('a[href*="#"]').click(function(){
+		var el = $(this).attr('href');
+		$('body').animate({
+			scrollTop: $(el).offset().top
+		}, 300);
+		return false;
+	});
 });
+
